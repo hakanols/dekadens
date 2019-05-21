64267ec03e5627e43c89d43ac877bece6c6b3bb3
Håkan Olsson <hakanols@gmail.com>
2019-05-21 23:40:17 +0200

    Update Cheat sheet after feedback

--- generator.py
+++ generator.py
@@ -51,24 +51,24 @@ backTemplate = """
 	  {% endfor -%}
 	  {% for t in range(numerOfCheatSheet) -%} 
 	  <div class="box box-sheet">
-	    <div class="inner inner-sheet" style="display: table;">
-		  <h1 style="display: table-row;">Samtycke</h1>
-		  <div  style="display: table-row; height:100%;">
-			<table style="height:100%; vertical-align: middle; text-align: center;">
-			  <tr>
-				<th><img src=Images/bird1.svg style="max-width:70%; height: auto;"></th>
-				<th><img src=Images/bird2.svg style="max-width:70%; height: auto;"></th>
-				<th><img src=Images/bird3.svg style="max-width:70%; height: auto;"></th>
-				<th><img src=Images/bird4.svg style="max-width:70%; height: auto;"></th>
-			  </tr>
-			  <tr>
-				<th><img src=Images/bird5.svg style="max-width:70%; height: auto;"></th>
-				<th><img src=Images/bird6.svg style="max-width:70%; height: auto;"></th>
-				<th><img src=Images/bird7.svg style="max-width:70%; height: auto;"></th>
-				<th><img src=Images/bird8.svg style="max-width:70%; height: auto;"></th>
-			  </tr>
-			</table>
-		  </div>
+	    <div class="inner inner-sheet"">
+		  <table style="height:100%; vertical-align: middle; text-align: center;">
+			<tr>
+			  <th><img src=Images/bird1.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird2.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird3.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird4.svg style="max-width:70%; height: auto;"></th>
+			</tr>
+			<tr>
+			  <td colspan = "4"><h1 style="margin: 0;">Samtycke</h1></td>
+			</tr>
+			<tr>
+			  <th><img src=Images/bird5.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird6.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird7.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird8.svg style="max-width:70%; height: auto;"></th>
+			</tr>
+		  </table>
 	    </div>
 	  </div>{% endfor -%}
 	</div>
@@ -123,13 +123,13 @@ frontTemplate = """
 	{% for t in range(numerOfCheatSheet) -%} 
 	<div class="box box-sheet"><div class="inner inner-sheet">
 	  <ul>
-        <li>Under sin tur har man ett val med dessa alternativ:</li>
+        <li>Under din tur välj ett av följande alternativ:</li>
         <ul>
           <li>Dra ett händelsekort</li>
-          <li>Få en <heart/> poäng</li>
-          <li>Försök att ligga med nått av personkorten på bordet</li>
+          <li>Gå till gymmet (+1<heart/>)</li>
+          <li>Försök att ligga med en person i staden (ej medspelare)</li>
         </ul>
-        <li>Ta ditt <heart/> minus personens <flake/> i tabbelen nedan för att se vilka tärningsslag som leder till lyckat ligg</li>
+        <li>Ta ditt <heart/> minus personens <flake/> i tabellen nedan för att se vilka tärningsslag som leder till lyckat ligg</li>
       </ul>
 	  <table class="dice">
         <tr>

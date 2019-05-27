4454fefad45296e796138c6ab7491d8ffac336f4
Håkan Olsson <hakanols@gmail.com>
2019-05-27 23:24:11 +0200

    Bug fix and cleanup. Updating release. Done for the moment.

--- generator.py
+++ generator.py
@@ -5,6 +5,72 @@ import json
 import pdfkit
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
+frontTemplate = """
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Game</title>
+	<meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+  </head>
+  <body class="main">
+	<div style="text-align:center;">
+	{% for cardType in cardTypes -%}
+	{% for card in cardType.cards -%}
+	{%for x in range(card.count) -%}
+	  <div class="box box-card" style="background-image: url({%if card.frontImage %}{{card.frontImage}}{% else %}{{cardType.frontImage}}{% endif %}); background-repeat:no-repeat; background-position: center; background-size: 80%;">
+		<div class="inner inner-card">
+		  <table style="width:100%; height: 100%; margin: 0px;">
+		    <tr style="height: 100%; vertical-align: top;">
+			  <td>
+			    <h4>{{ card.title }}</h4>
+			    <p>{{ card.description }}</p>
+			    {% for attribute in card.attributes -%}
+			    <h5>{{ attribute.title }}</h5>
+			    <p>{{ attribute.description }}</p>
+			    {% endfor -%}
+			    {% if cardType.template -%}
+				<br>
+			    {%for x in range(cardType.template|length)-%}
+			    <p style="padding-bottom: 20px;"><b>{{cardType.template[x]}}:</b> {{card['values'][x]}}</p>
+			    {% endfor -%}
+			    {% endif -%}
+			    {% if cardType.dispCount -%}
+			    <p><b>Ligg per spel:</b> {{card.count}}</p>
+			    {% endif -%}
+			  </td>
+			</tr>
+			{% if card.turns -%}
+			<tr style="height: 0;">
+			  <td>
+			    <table style="width:100%; text-align: center;">
+			   	  <tr>
+			   	    {% for t in range(card.turns) -%}
+			   	    <td><div class="circle"><span>{{t+1}}</span></div></td>
+			   	    {% endfor -%}
+			   	  </tr>
+			    </table>
+			  </td>
+			</tr>
+			{% endif -%}
+			{% if card.summation -%}
+			<tr style="height: 0;">
+			  <td>
+			    <p class="summation">{{card.summation}}</p>
+			  </td>
+			</tr>
+			{% endif -%}
+		  </table>
+		</div>
+	  </div>{% endfor -%}
+	  {% endfor -%}
+	  <br>
+	  {% endfor -%}
+	  {% for t in range(numerOfCheatSheet) -%}{{cheatSheet}}{% endfor -%}
+	</div>
+  </body>
+</html>
+"""
 
 backTemplate = """
 <!DOCTYPE html>
@@ -49,99 +115,13 @@ backTemplate = """
 	  {% endfor -%}
 	  <br>
 	  {% endfor -%}
-	  {% for t in range(numerOfCheatSheet) -%} 
-	  <div class="box box-sheet">
-	    <div class="inner inner-sheet"">
-		  <table style="height:100%; vertical-align: middle; text-align: center;">
-			<tr>
-			  <th><img src=Images/bird1.svg style="max-width:70%; height: auto;"></th>
-			  <th><img src=Images/bird2.svg style="max-width:70%; height: auto;"></th>
-			  <th><img src=Images/bird3.svg style="max-width:70%; height: auto;"></th>
-			  <th><img src=Images/bird4.svg style="max-width:70%; height: auto;"></th>
-			</tr>
-			<tr>
-			  <td colspan = "4"><h1 style="margin: 0;">Samtycke</h1></td>
-			</tr>
-			<tr>
-			  <th><img src=Images/bird5.svg style="max-width:70%; height: auto;"></th>
-			  <th><img src=Images/bird6.svg style="max-width:70%; height: auto;"></th>
-			  <th><img src=Images/bird7.svg style="max-width:70%; height: auto;"></th>
-			  <th><img src=Images/bird8.svg style="max-width:70%; height: auto;"></th>
-			</tr>
-		  </table>
-		</div>
-	  </div>{% endfor -%}
+	  {% for t in range(numerOfCheatSheet) -%}{{cheatSheet}}{% endfor -%}
 	</div>
   </body>
 </html>
 """
 
-
-frontTemplate = """
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Game</title>
-	<meta charset="UTF-8" />
-    <link rel="stylesheet" type="text/css" href="mystyle.css">
-  </head>
-  <body class="main">
-	<div style="text-align:center;">
-	{% for cardType in cardTypes -%}
-	{% for card in cardType.cards -%}
-	{%for x in range(card.count) -%}
-	  <div class="box box-card" style="background-image: url({%if card.frontImage %}{{card.frontImage}}{% else %}{{cardType.frontImage}}{% endif %}); background-repeat:no-repeat; background-position: center; background-size: 80%;">
-		<div class="inner inner-card">
-		  <table style="width:100%; height: 100%; margin: 0px;">
-		    <tr style="height: 100%; vertical-align: top;">
-			  <td>
-			    <h4>{{ card.title }}</h4>
-			    <p>{{ card.description }}</p>
-			    {% for attribute in card.attributes -%}
-			    <h5>{{ attribute.title }}</h5>
-			    <p>{{ attribute.description }}</p>
-			    {% endfor -%}
-			    {% if cardType.template -%}
-				<br>
-			    {%for x in range(cardType.template|length)-%}
-			    <p style="padding-bottom: 20px;"><b>{{cardType.template[x]}}:</b> {{card['values'][x]}}</p>
-			    {% endfor -%}
-			    {% endif -%}
-			    {% if cardType.dispCount -%}
-			    <p><b>Ligg per spel:</b> {{card.count}}</p>
-			    {% endif -%}
-			    {% if card.turns -%}
-			  </td>
-			</tr>
-			{% if card.turns -%}
-			<tr style="height: 0;">
-			  <td>
-			    <table style="width:100%; text-align: center;">
-			   	 <tr>
-			   	 {% for t in range(card.turns) -%}
-			   	 <th><div class="circle"><span>{{t+1}}</span></div></th>
-			   	 {% endfor -%}
-			   	 </tr>
-			    </table>
-			    {% endif -%}
-			  </td>
-			</tr>
-			{% endif -%}
-			{% if card.summation -%}
-			<tr style="height: 0;">
-			  <td>
-			    <p class="summation">{{card.summation}}</p>
-			  </td>
-			</tr>
-			{% endif -%}
-		  </table>
-		</div>
-	  </div>{% endfor -%}
-	  {% endfor -%}
-	  <br>
-	  {% endfor -%}
-	  {% for t in range(numerOfCheatSheet) -%} 
-	  <div class="box box-sheet">
+cheatSheetFront = """<div class="box box-sheet">
 		<div class="inner inner-sheet">
 		  <ul>
 		  	<li>Under din tur välj ett av följande alternativ:</li>
@@ -176,11 +156,29 @@ frontTemplate = """
 		    <li>Efter lyckat ligg tas ett utfallskort</li>
 		  </ul>
 		</div>
-	  </div>{% endfor -%}
-	</div>
-  </body>
-</html>
-"""
+	  </div>"""
+
+cheatSheetBack = """<div class="box box-sheet">
+	    <div class="inner inner-sheet"">
+		  <table style="height:100%; vertical-align: middle; text-align: center;">
+			<tr>
+			  <th><img src=Images/bird1.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird2.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird3.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird4.svg style="max-width:70%; height: auto;"></th>
+			</tr>
+			<tr>
+			  <td colspan = "4"><h1 style="margin: 0;">Samtycke</h1></td>
+			</tr>
+			<tr>
+			  <th><img src=Images/bird5.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird6.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird7.svg style="max-width:70%; height: auto;"></th>
+			  <th><img src=Images/bird8.svg style="max-width:70%; height: auto;"></th>
+			</tr>
+		  </table>
+		</div>
+	  </div>"""
 
 def debug(text):
     print(text)
@@ -194,18 +192,17 @@ def read_json_file():
 
         environment = Environment()
         environment.filters['debug']=debug
-        frontPages = environment.from_string(frontTemplate).render(cardTypes=data, numerOfCheatSheet=numerOfCheatSheet)
+        frontPages = environment.from_string(frontTemplate).render(cardTypes=data, cheatSheet=cheatSheetFront, numerOfCheatSheet=numerOfCheatSheet)
         with open('fronts.html', 'w', encoding='utf8') as outfile:  
             outfile.write(frontPages)
 		
-        backPages = Environment().from_string(backTemplate).render(cardTypes=data, numerOfCheatSheet=numerOfCheatSheet)
+        backPages = Environment().from_string(backTemplate).render(cardTypes=data, cheatSheet=cheatSheetBack, numerOfCheatSheet=numerOfCheatSheet)
         with open('backs.html', 'w', encoding='utf8') as outfile:  
             outfile.write(backPages)
 
 def pdf_from_html():
     path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
     config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
-    #pdfkit.from_string('Game', 'out.pdf', configuration=config)
     pdfkit.from_file('fronts.html', 'fronts.pdf', configuration=config)
     pdfkit.from_file('backs.html', 'backs.pdf', configuration=config)
 	
